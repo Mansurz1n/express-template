5ad32a0ed834654276b141f8aa12d18083315520
GABRIEL DE OLIVEIRA MANSUR <gabriel.om4@puccampinas.edu.br>
2024-10-28 08:29:40 -0300

    comitando algo que fiz em menos de 2 min para outra pessoar pegar o codigo

---
+++ src/wallet/wallet.ts
@@ -0,0 +1,13 @@
+import {Request, RequestHandler, Response} from "express";
+import OracleDB from "oracledb"
+
+
+
+
+export namespace WalletHandler 
+{
+    export const addfunds:RequestHandler = (req:Request, res:Response) =>
+    {
+        
+    }
+}
